8491ef3ff1f386b042796ea4dad355c61953d1b9
qwe5507 <apvmffkdls@gmail.com>
2022-09-10 17:25:19 +0900

    Pessimistic Lock 적용하여 race condition해결
    Pessimistic Lock의 장점
    - 충돌이빈번하게 일어난다면 Optimistic Lock보다 성능이 좋을 수 있다.
    - Lock을통해 Update를 제어하기 때문에 데이터 정합성이 어느정도 보장된다.
    Pessimistic Lock의 단점
    - 별도의 Lock을걸기때문에 성능 감소가 있을 수 있다.

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -2,7 +2,15 @@ package com.example.stock.repository;
 
 import com.example.stock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.LockModeType;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    //Spring Data Jpa는 @Lock을 통해 Pessimistic Lock을 설정 할 수있다.
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPassimisticLock(Long id);
 }
---
+++ src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,25 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+
+    private StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPassimisticLock(id); //lock을 걸고 데이터를 가져온다.
+
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+}
