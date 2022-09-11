7a1b3c230dbe36a86d05a4f9a44b8f40e102bf15
qwe5507 <apvmffkdls@gmail.com>
2022-09-11 15:52:51 +0900

    Named Lock 적용하여 race condition해결
    NamedLock은 분산 Lock을 구현할 떄 주로 사용 한다.(?)
    - Pessimistic Lock은 타임아웃을 구현하기 힘들지만 NamedLock은 손쉽게 구현할 수있다.
    NamedLock은 트랜잭션 종료 시에 Lock해체와 세션관리를 잘해줘야 하므로, 주의해서 사용해야 하고, 실제로 사용할때는 구현방식이 복잡할수 있다.

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,15 +15,11 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    // 스프링의 @Transactional은 프록시 객체를 만든다.
-    // 프록시 객체의 타겟의 로직실행 -> 트랜잭션 종료 그 사이에 다른 요청의 타겟의 로직이 실행될 수 있다.
-    // 테스트 통과를 위해 @Transactional을 주석처리 한다.
-//    @Transactional
-    public synchronized void decrease(Long id, Long quantity) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
         // get stock
         // 재고 감소
         // 저장
-
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
         stockRepository.saveAndFlush(stock);
