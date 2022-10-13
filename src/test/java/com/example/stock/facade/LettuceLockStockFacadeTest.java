c81f067a2359f6d59261374989d8f8fa0145dbf6
qwe5507 <apvmffkdls@gmail.com>
2022-10-13 22:39:24 +0900

    Redis를 Lettuce라이브러리를 활용하여 동시성 제어 - 테스트 코드

--- src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
+++ src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
@@ -1,7 +1,62 @@
 package com.example.stock.facade;
 
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class LettuceLockStockFacadeTest {
+    @Autowired
+    private LettuceLockStockFacade lettuceLockStockFacade;
 
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    lettuceLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown(); //countDown()을 호출하면 Latch의 숫자가 1개씩 감소한다.
+                }
+            });
+        }
+        latch.await(); //await()은 Latch의 숫자가 0이 될 때까지 기다리는 메서드
+        //latch의 숫자가 0일될떄까지 이 시점에서 기다리다가 그 후 진행한다.
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        //예상 : 100 - (1 * 100) = 0
+        assertThat(stock.getQuantity()).isEqualTo(0);
+    }
 }
